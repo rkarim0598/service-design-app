f9d6fd63c2993bb9c79f46d2a2a443408564d1a5
Rayyan Karim <rkarim0598@gmail.com>
2019-03-14 03:55:37 -0700

    Actually use local storage now

--- src/app/confirm-appt/confirm-appt.component.ts
+++ src/app/confirm-appt/confirm-appt.component.ts
@@ -9,9 +9,14 @@ export class ConfirmApptComponent implements OnInit {
   location = localStorage.getItem('location') != 'undefined' ? localStorage.getItem('location') : 'Select a store!';
   date = localStorage.getItem('date') != 'undefined' ? localStorage.getItem('date') : "Select a date";
   time = localStorage.getItem('time') != 'undefined' ? localStorage.getItem('time') : "Select a time slot!";
+  submitted = false;
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  submit() {
+    this.submitted = true;
+  }
 }
